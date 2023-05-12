ded5c3b20d27e6192a9287195505874a9da4050a
Vanessa O <vaneolipa@gmail.com>
2023-05-12 17:33:29 -0500

    Add after_destroy methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,15 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
-  after_create :update_likes_counter
+  after_save :update_likes_counter
+  after_destroy :decrease_likes_counter
 
   # A method that updates the likes counter for a post.
   def update_likes_counter
     post.increment!(:likes_counter)
+  end
+
+  def decrease_likes_counter
+    post.decrement!(:likes_counter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy, foreign_key: :post_id
   has_many :likes, dependent: :destroy, foreign_key: :post_id
   after_save :update_posts_counter
+  after_destroy :decrease_posts_counter
 
   validates :title, presence: true, length: { in: 2..100 }
   validates :text, presence: true, length: { in: 10..600 }
@@ -12,6 +13,10 @@ class Post < ApplicationRecord
   # A method that updates the posts counter for a user.
   def update_posts_counter
     author.increment!(:posts_counter)
+  end
+
+  def decrease_posts_counter
+    author.decrement!(:posts_counter)
   end
 
   # A method which returns the 5 most recent comments for a given post.
