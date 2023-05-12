04433db189b78f1f9e39dea849eb307e2885e7e0
Vanessa O <vaneolipa@gmail.com>
2023-05-12 17:03:54 -0500

    Add recent_posts method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
+  has_many :posts, dependent: :destroy, foreign_key: :author_id
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   validates :photo, presence: true
   validates :bio, presence: true, length: { in: 10..250 }
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def recent_posts
+    posts.last(3)
+  end
 end
