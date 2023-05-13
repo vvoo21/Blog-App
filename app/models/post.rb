f1d733da2293b720c365c0d47a36408b80576f6e
Vanessa O <vaneolipa@gmail.com>
2023-05-13 17:17:43 -0500

    Add validations For the User and Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,16 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
   after_destroy :decrease_posts_counter
 
-  validates :title, presence: true, length: { in: 2..100 }
+  validates :title, presence: true, length: { in: 2..250 }
   validates :text, presence: true, length: { in: 10..600 }
-  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates_numericality_of :comments_counter, only_integer: true, greater_than_or_equal_to: 0
+  validates_numericality_of :likes_counter, only_integer: true, greater_than_or_equal_to: 0
 
+  # A method which returns the 5 most recent comments for a given post.
+  def recent_comments
+    comments.last(5)
+  end
+  
   # A method that updates the posts counter for a user.
   def update_posts_counter
     author.increment!(:posts_counter)
@@ -17,10 +22,5 @@ class Post < ApplicationRecord
 
   def decrease_posts_counter
     author.decrement!(:posts_counter)
-  end
-
-  # A method which returns the 5 most recent comments for a given post.
-  def recent_comments
-    comments.last(5)
   end
 end
