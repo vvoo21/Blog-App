ea7b4e297a59dc0d086f35fbe409fa5c7327a782
Vanessa O <vaneolipa@gmail.com>
2023-05-20 13:55:04 -0500

    Revert "Merge pull request #4 from vvoo21/controllers"
    This reverts commit 7a6e0c3fd52369e4365a60e7429ce4acbf4cf0ab, reversing
    changes made to 97ceaff0293e8781c9f476bdd1d1c1ea8911579c.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root "users#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users do
-    resources :posts
-  end
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
