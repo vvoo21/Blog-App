bf12c29ddcbb197d044df8b16c21f7c0a7f4e2ed
Vanessa O <vaneolipa@gmail.com>
2023-05-20 03:06:19 -0500

    Add routes, actions and views for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "users#index"
 
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
+  resources :users do
+    resources :posts
+  end
 end
