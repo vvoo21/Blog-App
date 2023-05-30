91710670d16183d049f39b5049197bae070ae5e4
Vanessa <vaneolipa@gmail.com>
2023-05-30 17:47:05 -0500

    Create files for add test case in User pages

---
+++ spec/integration/user_index_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe 'User Index Page', type: :feature do
+end
---
+++ spec/integration/user_show_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe 'User Show Page', type: :feature do
+ 
+end
