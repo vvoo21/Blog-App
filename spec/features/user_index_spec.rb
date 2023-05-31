dcb7e43ee68b0fa833262c37d6efa63d979d4db3
Vanessa <vaneolipa@gmail.com>
2023-05-31 11:13:16 -0500

    Using Capybara with RSpec

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -8,33 +8,36 @@ RSpec.describe 'User Index Page', type: :feature do
     bio: 'A modern artist',
     posts_counter: 5
   )
-
-  visit users_path
   end
 
   it 'displays a container for the users' do
+    visit users_path
     expect(page).to have_css('section.users-section')
   end
 
   it 'displays the username of each user' do
+    visit users_path
     User.all.each do |user|
       expect(page).to have_content(user.name)
     end
   end
 
   it 'displays the photos of each user' do
+    visit users_path
     User.all.each do |user|
       expect(page.has_xpath?("//img[@src = '#{user.photo}' ]"))
     end
   end
 
   it 'shows the number of posts of each user' do
+    visit users_path
     User.all.each do |user|
       expect(page).to have_content("Number of posts: #{user.posts_counter}")
     end
   end
 
   it "is redirected to that user's show page" do
+    visit users_path
     click_link(@user.name)
     expect(page).to have_current_path(user_path(@user.id))
   end
